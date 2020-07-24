807f111c86affba3f6ff64a24b9735771502c0cd
Lukas Wais <lukas.wais@outlook.de>
2020-07-24 09:54:21 +0200

    added openCV

--- src/main/java/lukas/wais/aibooth/App.java
+++ src/main/java/lukas/wais/aibooth/App.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.opencv.core.Core;
 
 import java.io.IOException;
 
@@ -32,6 +33,8 @@ public class App extends Application {
     }
 
     public static void main(String[] args) {
+        // load the native OpenCV library
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
         launch();
     }
 
